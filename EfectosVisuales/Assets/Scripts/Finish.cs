752a202a3c21a2af1f239ac3e08eae8b3a2ef404
Lorenyedi <lorenyedi3@gmail.com>
2025-06-25 03:41:00 -0300

    Commit entrega

--- EfectosVisuales/Assets/Scripts/Finish.cs
+++ EfectosVisuales/Assets/Scripts/Finish.cs
@@ -1,24 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Finish : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField] GameObject fondo, boton01, boton02;
+    
     public void Salir()
     {
         Application.Quit();
     }
     public void Retry()
     {
+        fondo.SetActive(false);
+        boton01.SetActive(false);
+        boton02.SetActive(false);
         SceneManager.LoadScene(0);
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other == EntityManager.instance.player)
+        if (other.name == "Player")
         {
-
+            fondo.SetActive(true);
+            boton01.SetActive(true);
+            boton02.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
     }
 }
